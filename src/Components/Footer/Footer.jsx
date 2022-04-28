66eb65c672c3e0aaa55942f8d287ea80b64ee9c7
rhkearns <ryankearns217@gmail.com>
2022-04-28 10:39:43 -0400

    footer fill and initial style

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import './Footer.scss'
 
 const Footer = () => {
-  return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  );
+	return (
+		<div id="footer">
+      <div className='blurb'>
+        <p>Based in New York, NY</p>
+        <p>Avaiable anywhere virtually</p>
+      </div>
+      <div id="icons">
+				<img src="" alt="Instagram" />
+				<img src="" alt="Facebook" />
+				<img src="" alt="Twitter" />
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
