13efcd5a45e2fd0b6ffdaee844ad6a264b9d0bc4
rhkearns <ryankearns217@gmail.com>
2022-04-28 13:28:58 -0400

    add socials icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import './Footer.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faInstagram,
+	faFacebookF,
+	faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+import './Footer.scss';
 
 const Footer = () => {
 	return (
 		<div id="footer">
-      <div className='blurb'>
-        <p>Based in New York, NY</p>
-        <p>Avaiable anywhere virtually</p>
-      </div>
-      <div id="icons">
-				<img src="" alt="Instagram" />
-				<img src="" alt="Facebook" />
-				<img src="" alt="Twitter" />
+			<div className="blurb">
+				<p>Based in New York, NY</p>
+				<p>Avaiable anywhere virtually</p>
+			</div>
+			<div id="icons">
+				<FontAwesomeIcon icon={faInstagram} />
+				<FontAwesomeIcon icon={faFacebookF} />
+				<FontAwesomeIcon icon={faTwitter} />
 			</div>
 		</div>
 	);
