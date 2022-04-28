24bf81b15bee8c374e161cf871e4ab1693d60258
rhkearns <ryankearns217@gmail.com>
2022-04-28 13:05:25 -0400

    header fill and initial style

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import './Header.scss'
 
 const Header = () => {
-  return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  );
+	return (
+		<div id="header">
+			<img src="" alt="Streamlined Partners Logo" />
+			<div id="title">
+				<h1>STREAMLINED</h1>
+				<h1>PARTNERS</h1>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
