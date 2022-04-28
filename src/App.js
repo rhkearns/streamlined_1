c2176a0d5460ec55315a9998516866fb9ee9f042
rhkearns <ryankearns217@gmail.com>
2022-04-28 10:31:42 -0400

    import components into app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header'
+import ContentBody from './Components/ContentBody/ContentBody'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <ContentBody />
+      <Contact />
+      <Footer />
     </div>
   );
 }
