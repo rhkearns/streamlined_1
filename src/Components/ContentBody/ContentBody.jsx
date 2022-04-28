af41db4aaef7007efe69b02e01c9de974f080f63
rhkearns <ryankearns217@gmail.com>
2022-04-28 10:56:01 -0400

    content body fill and initial style

--- src/Components/ContentBody/ContentBody.jsx
+++ src/Components/ContentBody/ContentBody.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './ContentBody.scss'
 
 const ContentBody = () => {
   return (
-    <div>
-      <h1>ContentBody</h1>
+    <div id="content">
+      <h2 id="top-line">We work with small businesses and entrepreneurs to get them set up for success AND STAY ON TRACK</h2>
+      <p>Streamlined Partners is your full service logistics and operations resource. We are your on demand COO, ready to come in and help you move forwad in all areas of systemization, technology, and project management.</p>
+      <p>We can show you how to adjust and move forward to make the best of your resources, budget, and talent.</p>
+      <h4 id="bottom-line">Full services coming soon</h4>
     </div>
   );
 };
