5c0f5c65b4fa5e8f378f77d883da2ff956a5a300
rhkearns <ryankearns217@gmail.com>
2022-05-12 13:31:36 -0400

    clean up

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Header.scss';
 import icon from '../../images/Icon.png';
-import logoText from '../../images/Logo_Text.png';
 import streamlined from '../../images/Streamline_Tight.png';
 import partners from '../../images/Partners_Tight.png';
 
@@ -9,7 +8,6 @@ const Header = () => {
 	return (
 		<div id="header">
 			<img src={icon} alt="Streamlined Partners Logo" id="icon" />
-			{/* <img src={logoText} alt="Streamlined Partners" id="logoText"/> */}
 			<div id="title">
 				<img src={streamlined} alt="Streamlined" id="streamlined" />
 				<img src={partners} alt="Partners" id="partners" />
