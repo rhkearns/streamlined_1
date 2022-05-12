96ee7131b0dcc8f17cc6eb2e5be096adf3136427
rhkearns <ryankearns217@gmail.com>
2022-05-12 13:31:01 -0400

    add and style logo, responsive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import './Header.scss'
+import './Header.scss';
+import icon from '../../images/Icon.png';
+import logoText from '../../images/Logo_Text.png';
+import streamlined from '../../images/Streamline_Tight.png';
+import partners from '../../images/Partners_Tight.png';
 
 const Header = () => {
 	return (
 		<div id="header">
-			<img src="" alt="Streamlined Partners Logo" />
+			<img src={icon} alt="Streamlined Partners Logo" id="icon" />
+			{/* <img src={logoText} alt="Streamlined Partners" id="logoText"/> */}
 			<div id="title">
-				<h1>STREAMLINED</h1>
-				<h1>PARTNERS</h1>
+				<img src={streamlined} alt="Streamlined" id="streamlined" />
+				<img src={partners} alt="Partners" id="partners" />
 			</div>
 		</div>
 	);
