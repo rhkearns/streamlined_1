ee8f80f816233621eb031c8649c4306e601ffa49
rhkearns <ryankearns217@gmail.com>
2022-05-14 17:05:31 -0400

    header futzing

--- src/Components/ContentBody/ContentBody.jsx
+++ src/Components/ContentBody/ContentBody.jsx
@@ -9,10 +9,9 @@ const ContentBody = () => {
 				success and stay on track
 			</h2>
 			<p>
-				Streamlined Partners is here to assist you in moving forward 
-				in all areas of systemization, strategizing, and accountability. 
+				Streamlined Partners is here to assist you in moving forward in all
+				areas of systemization, strategizing, and accountability.
 				{/* We specialize in project management and organization. */}
-
 			</p>
 			<p>
 				We will show you how to adjust and move forward to make the best of your
@@ -22,31 +21,34 @@ const ContentBody = () => {
 			{/* <h4 id="bottom-line">Full services coming soon!</h4> */}
 			<div className="services">
 				<div id="systemization">
-          <h5>Systemization</h5>
-          <ul>
+					<h5>Systemization</h5>
+					<ul>
 						<li>Domain creation</li>
 						<li>Website set up</li>
 						<li>Email/Google Workspace set up</li>
 						<li>Work management software implementation</li>
 						<li>Calendar scheduling and syncing</li>
-            {/* <li>Financial infrastructure set up</li> */}
-            <li>Full system troublshooting</li>
+						{/* <li>Financial infrastructure set up</li> */}
+						<li>Full system troublshooting</li>
 					</ul>
 				</div>
 				<div id="strategizing">
-          <h5>Strategizing</h5>
-          <ul>
+					<h5>Strategizing</h5>
+					<ul>
 						<li>Consulting</li>
 						<li>Sounding board for ideas</li>
 						<li>Figure out what you need</li>
 						<li>Research, outreach, and connections</li>
-						<li>Index/directory/catalogue of tech & work management programs to find the best option for you</li>
+						<li>
+							Index/directory/catalogue of tech & work management programs to
+							find the best option for you
+						</li>
 						<li>Network of freelancers for specialized projects</li>
 					</ul>
 				</div>
 				<div id="onboarding">
-          <h5>Onboarding</h5>
-          <ul>
+					<h5>Onboarding</h5>
+					<ul>
 						<li>Assess needs for future hires</li>
 						<li>Assist with job description creation</li>
 						<li>Onboard new hires </li>
@@ -54,13 +56,16 @@ const ContentBody = () => {
 					</ul>
 				</div>
 				<div id="accountability">
-          <h5>Accountability</h5>
-          <ul><li>We provide you the tools you need to hold yourself and your team accoutable</li>
+					<h5>Accountability</h5>
+					<ul>
+						<li>
+							We provide you the tools you need to hold yourself and your team
+							accoutable
+						</li>
 						<li>Keep projects moving</li>
 						<li>Timeline creation</li>
 						<li>Deadline Monitoring</li>
 						<li>KPI monitoring</li>
-						
 					</ul>
 				</div>
 			</div>
