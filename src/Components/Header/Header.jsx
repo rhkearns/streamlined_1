f65f739916e03c2674ad458a0eebca67f2f4cd69
rhkearns <ryankearns217@gmail.com>
2022-05-14 17:24:17 -0400

    replace logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,15 +3,17 @@ import './Header.scss';
 import icon from '../../images/Icon.png';
 import streamlined from '../../images/Streamline_Tight.png';
 import partners from '../../images/Partners_Tight.png';
+import logo from '../../images/full_logo.png'
 
 const Header = () => {
 	return (
 		<div id="header">
-			<img src={icon} alt="Streamlined Partners Logo" id="icon" />
+			{/* <img src={icon} alt="Streamlined Partners Logo" id="icon" />
 			<div id="title">
 				<img src={streamlined} alt="Streamlined" id="streamlined" />
 				<img src={partners} alt="Partners" id="partners" />
-			</div>
+			</div> */}
+			<img src={logo} alt="Streamlined Partners" id="logo"/>
 		</div>
 	);
 };
