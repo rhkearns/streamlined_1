97dde922f4a00d0532be94c5a97c6241ade85991
rhkearns <ryankearns217@gmail.com>
2022-05-14 17:25:03 -0400

    clean up

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import './Header.scss';
-import icon from '../../images/Icon.png';
-import streamlined from '../../images/Streamline_Tight.png';
-import partners from '../../images/Partners_Tight.png';
-import logo from '../../images/full_logo.png'
+import logo from '../../images/full_logo.png';
 
 const Header = () => {
 	return (
 		<div id="header">
-			{/* <img src={icon} alt="Streamlined Partners Logo" id="icon" />
-			<div id="title">
-				<img src={streamlined} alt="Streamlined" id="streamlined" />
-				<img src={partners} alt="Partners" id="partners" />
-			</div> */}
-			<img src={logo} alt="Streamlined Partners" id="logo"/>
+			<img src={logo} alt="Streamlined Partners" id="logo" />
 		</div>
 	);
 };
