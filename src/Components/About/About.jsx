5487ded38988ed9e8397af3d14f08ddd135259c1
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2022-05-15 16:08:54 -0400

    About update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -12,8 +12,7 @@ const About = () => {
       adminsitrative, executive, and personal assistant. She has worked for private 
       clients and companies, including Bang & Olufsen, Libero Canto School of Singing, 
       Mood Bars, Unf*ck Your Brain, Overflow, Thinkertales, Arena Holdings, Cultural Road Mapp, 
-      Time Inc., ArtMuse, and the Bellevue Association. Samantha also ran her own CPG company for 
-      5 years before prior to launching Streamlined Partners. The Streamlined Partners team has been 
+      Time Inc., ArtMuse, and the Bellevue Association. Samantha also ran her own CPG company prior to launching Streamlined Partners. The Streamlined Partners team has been 
       growing with experienced assistants, project managers, and operations managers that are ready to 
       work with you and your business.</p> </div>
       </div>
