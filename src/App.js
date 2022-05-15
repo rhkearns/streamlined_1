8fe0c8ae83c3caa94145302664180e0427e4f00c
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2022-05-15 15:32:42 -0400

    About CSS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 import Nav from './Components/Nav/Nav';
 import BottomNav from './Components/BottomNav/BottomNav';
-// import About from './Components/About/About';
+import About from './Components/About/About';
 // import Testimonials from './Components/Testimonials/Testimonials';
 
 function App() {
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,9 +3,11 @@ import './About.scss'
 
 const About = () => {
   return (
-    <div id='about-section'>
-      <h1>ABOUT</h1>
-      Streamlined Partners is a boutque consulting firm founded by Samantha Yudin. 
+    <div className='about-section'>
+      <div id="about-header">
+        <h1>ABOUT</h1></div>
+      <div id="about-blurb">
+        <p> Streamlined Partners is a boutque consulting firm founded by Samantha Yudin. 
       Samantha has over a decade of experience working as a project manager and 
       adminsitrative, executive, and personal assistant. She has worked for private 
       clients and companies, including Bang & Olufsen, Libero Canto School of Singing, 
@@ -13,9 +15,9 @@ const About = () => {
       Time Inc., ArtMuse, and the Bellevue Association. Samantha also ran her own CPG company for 
       5 years before prior to launching Streamlined Partners. The Streamlined Partners team has been 
       growing with experienced assistants, project managers, and operations managers that are ready to 
-      work with you and your business.
-    </div>
+      work with you and your business.</p>
+      </div></div>
   );
-};
+  }
 
 export default About;
