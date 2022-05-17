ba75a989eeaf21a515423df8cf9d2ecddc695177
rhkearns <ryankearns217@gmail.com>
2022-05-17 15:19:47 -0400

    alignment futzing

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,24 +4,21 @@ import './About.scss';
 const About = () => {
 	return (
 		<div className="about-section">
-			<div id="about-header">
-				<h1>About</h1>
-				<div id="about-blurb">
-					<p>
-						{' '}
-						Streamlined Partners is a boutque consulting firm founded by
-						Samantha Yudin. Samantha has over a decade of experience working as
-						a project manager and adminsitrative, executive, and personal
-						assistant. She has worked for private clients and companies,
-						including Bang & Olufsen, Libero Canto School of Singing, Mood Bars,
-						Unf*ck Your Brain, Overflow, Thinkertales, Arena Holdings, Cultural
-						Road Mapp, Time Inc., ArtMuse, and the Bellevue Association.
-						Samantha also ran her own CPG company prior to launching Streamlined
-						Partners. The Streamlined Partners team has been growing with
-						experienced assistants, project managers, and operations managers
-						that are ready to work with you and your business.
-					</p>{' '}
-				</div>
+			<h1 id="about-header">About</h1>
+			<div id="about-blurb">
+				<p>
+					Streamlined Partners is a boutque consulting firm founded by Samantha
+					Yudin. Samantha has over a decade of experience working as a project
+					manager and adminsitrative, executive, and personal assistant. She has
+					worked for private clients and companies, including Bang & Olufsen,
+					Libero Canto School of Singing, Mood Bars, Unf*ck Your Brain,
+					Overflow, Thinkertales, Arena Holdings, Cultural Road Mapp, Time Inc.,
+					ArtMuse, and the Bellevue Association. Samantha also ran her own CPG
+					company prior to launching Streamlined Partners. The Streamlined
+					Partners team has been growing with experienced assistants, project
+					managers, and operations managers that are ready to work with you and
+					your business.
+				</p>
 			</div>
 		</div>
 	);
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,41 +6,40 @@ const Contact = () => {
 		<div className="contact-section">
 			<div id="contact-header">
 				<h1>Contact Us</h1>
-				<div id="contact-blurb">
-					<p>
-						Want more information? Want to get started but don't know where to
-						start?
-					</p>
-					<p>
-						We're not coaches, we aren't going to give you a free worksheet "3
-						tricks to staying organized." We will sit down with you and go over
-						your business and figure out what we can do to help you get and stay
-						on track. Our consultation is free, and we will create a plan based
-						on your needs. Set up your consultation today to get started!
-					</p>
-					<p>
-						Contact us by filling out the{' '}
-						<a
-							href="https://airtable.com/embed/shrggi2IoXGexh59K?backgroundColor=cyan"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							{' '}
-							form here
-						</a>{' '}
-						or emailing us at{' '}
-						<a
-							href="mailto:info@streamlinedpartners.com"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							{' '}
-							info@streamlinedpartners.com.
-						</a>
-					</p>
-				</div>
 			</div>
-			<hr className="line" />
+			<div id="contact-blurb">
+				<p>
+					Want more information? Want to get started but don't know where to
+					start?
+				</p>
+				<p>
+					We're not coaches, we aren't going to give you a free worksheet "3
+					tricks to staying organized." We will sit down with you and go over
+					your business and figure out what we can do to help you get and stay
+					on track. Our consultation is free, and we will create a plan based on
+					your needs. Set up your consultation today to get started!
+				</p>
+				<p>
+					Contact us by filling out the{' '}
+					<a
+						href="https://airtable.com/embed/shrggi2IoXGexh59K?backgroundColor=cyan"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{' '}
+						form here
+					</a>{' '}
+					or emailing us at{' '}
+					<a
+						href="mailto:info@streamlinedpartners.com"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{' '}
+						info@streamlinedpartners.com.
+					</a>
+				</p>
+			</div>
 		</div>
 	);
 };
