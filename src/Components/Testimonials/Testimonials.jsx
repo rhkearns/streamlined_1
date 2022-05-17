2489c276e963c0676692baa85906d8074bbea9c6
rhkearns <ryankearns217@gmail.com>
2022-05-17 15:23:58 -0400

    add carly testimonial

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Testimonials.scss';
 import { Carousel } from 'react-bootstrap';
-import template from '../../images/testimonials/template.jpg';
+import carly from "../../images/testimonials/Testimonial_Carly.png"
 
 const Testimonials = () => {
 	return (
@@ -9,13 +9,13 @@ const Testimonials = () => {
 			<h1>Testimonials</h1>
 			<Carousel variant="dark">
 				<Carousel.Item>
-					<img className="" src={template} alt="template" />
+					<img src={carly} alt="Mood Bars" />
 				</Carousel.Item>
 				<Carousel.Item>
-					<img className="" src={template} alt="template" />
+					<img src={carly} alt="template" />
 				</Carousel.Item>
 				<Carousel.Item>
-					<img className="" src={template} alt="template" />
+					<img src={carly} alt="template" />
 				</Carousel.Item>
 			</Carousel>
 		</div>
