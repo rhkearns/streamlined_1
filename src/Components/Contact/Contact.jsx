9533df285982a8b4bd4feac4c6176602e93658b4
rhkearns <ryankearns217@gmail.com>
2022-05-30 18:05:26 -0400

    image in contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,43 +1,44 @@
 import React from 'react';
 import './Contact.scss';
+import hands from '../../images/hands.jpg';
 
 const Contact = () => {
 	return (
-		<div className="contact-section">
-			<h1 id="contact-header">Contact Us</h1>
-			<div id="contact-blurb">
-				<p>
-					Want more information? Want to get started but don't know where to
-					start?
-				</p>
-				<p>
-					We're not coaches, we aren't going to give you a free worksheet "3
-					tricks to staying organized." We will sit down with you and go over
-					your business and figure out what we can do to help you get and stay
-					on track. Our consultation is free, and we will create a plan based on
-					your needs. Set up your consultation today to get started!
-				</p>
-				<p>
-					Contact us by filling out the{' '}
-					<a
-						href="https://airtable.com/embed/shrggi2IoXGexh59K?backgroundColor=cyan"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						{' '}
-						form here
-					</a>{' '}
-					or emailing us at{' '}
-					<a
-						href="mailto:info@streamlinedpartners.com"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						{' '}
-						info@streamlinedpartners.com.
-					</a>
-				</p>
+		<div className="outer-section">
+			<div className="contact-section">
+				<h1 id="contact-header">Contact Us</h1>
+				<div id="contact-blurb">
+					<p>
+						Want more information? Want to get started but don't know where to
+						start?
+					</p>
+					<p>
+						Contact us today to schedule a free consultation where we will sit
+						down with you and create a plan based on your needs.
+					</p>
+					<p>
+						Fill out this{' '}
+						<a
+							href="https://airtable.com/embed/shrggi2IoXGexh59K?backgroundColor=cyan"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							{' '}
+							contact form
+						</a>{' '}
+						or email us at{' '}
+						<a
+							href="mailto:info@streamlinedpartners.com"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							{' '}
+							info@streamlinedpartners.com.
+						</a>
+					</p>
+				</div>
 			</div>
+				<img src={hands} alt="hands" id="hands"/>
 		</div>
 	);
 };
