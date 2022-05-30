35d9a06ebfe6e886d60ca7a8d2b053f186ef8281
rhkearns <ryankearns217@gmail.com>
2022-05-30 16:00:22 -0400

    bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './Components/Header/Header'
 import ContentBody from './Components/ContentBody/ContentBody'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
-import Nav from './Components/Nav/Nav';
+// import Nav from './Components/Nav/Nav';
 import About from './Components/About/About';
 import Testimonials from './Components/Testimonials/Testimonials';
 
