2def30494f908f08c7cd80da9872e1e9b3a8f09a
rhkearns <ryankearns217@gmail.com>
2022-05-31 12:20:28 -0400

    carousel good enough

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import './Testimonials.scss';
 import { Carousel } from 'react-bootstrap';
-import carly from "../../images/testimonials/Testimonial_Carly.png"
+import testimonialData from '../../common/testimonialData';
+import Testimonial_Carly from '../../images/testimonials/Testimonial_Carly.png'
 
 const Testimonials = () => {
+	console.log(testimonialData);
+	const findImage = image => {
+		switch (image) {
+			case 'carly':
+				return Testimonial_Carly;
+			default:
+				break;
+		}
+	}
 	return (
 		<div id="testimonials">
 			<h1>Testimonials</h1>
 			<Carousel variant="dark">
-				<Carousel.Item>
-					<img src={carly} alt="Mood Bars" />
-				</Carousel.Item>
-				<Carousel.Item>
-					<img src={carly} alt="template" />
-				</Carousel.Item>
-				<Carousel.Item>
-					<img src={carly} alt="template" />
-				</Carousel.Item>
+				{testimonialData.map(testimonial => (
+					<Carousel.Item>
+						<img className='background' src={findImage(testimonial.imgTitle)} alt={testimonial.body} />
+					</Carousel.Item>
+				))}
 			</Carousel>
 		</div>
 	);
--- src/common/testimonialData.js
+++ src/common/testimonialData.js
@@ -2,8 +2,9 @@ const testimonialData = [
 	{
 		name: 'Carly S.',
 		company: 'Mood Bars',
-		body: 'Samantha from Streamlined Partners is truly the best - she is proactive, organized, effective, and incredibly helpful. Working with Samantha has been seamless and incredibly helpful. She has mad my life so much easier and my business so much stronger.',
-	},
+    body: 'Samantha from Streamlined Partners is truly the best - she is proactive, organized, effective, and incredibly helpful. Working with Samantha has been seamless and incredibly helpful. She has mad my life so much easier and my business so much stronger.',
+    imgTitle: "carly",
+  },
 ];
 
 export default testimonialData;
