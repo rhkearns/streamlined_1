99a5c22ebbed08ad6d3f712e156420d230015cb7
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2022-06-01 13:29:37 -0400

    hide testimonials

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import './Testimonials.scss';
-import { Carousel } from 'react-bootstrap';
-import testimonialData from '../../common/testimonialData';
-import Testimonial_Carly from '../../images/testimonials/Testimonial_Carly.png'
+// import React from 'react';
+// import './Testimonials.scss';
+// import { Carousel } from 'react-bootstrap';
+// import testimonialData from '../../common/testimonialData';
+// import Testimonial_Carly from '../../images/testimonials/Testimonial_Carly.png'
 
-const Testimonials = () => {
-	console.log(testimonialData);
-	const findImage = image => {
-		switch (image) {
-			case 'carly':
-				return Testimonial_Carly;
-			default:
-				break;
-		}
-	}
-	return (
-		<div id="testimonials">
-			<h1>Testimonials</h1>
-			<Carousel variant="dark">
-				{testimonialData.map(testimonial => (
-					<Carousel.Item>
-						<img className='testimonial-img' src={findImage(testimonial.imgTitle)} alt={testimonial.body} />
-					</Carousel.Item>
-				))}
-			</Carousel>
-		</div>
-	);
-};
+// const Testimonials = () => {
+// 	console.log(testimonialData);
+// 	const findImage = image => {
+// 		switch (image) {
+// 			case 'carly':
+// 				return Testimonial_Carly;
+// 			default:
+// 				break;
+// 		}
+// 	}
+// 	return (
+// 		<div id="testimonials">
+// 			<h1>Testimonials</h1>
+// 			<Carousel variant="dark">
+// 				{testimonialData.map(testimonial => (
+// 					<Carousel.Item>
+// 						<img className='testimonial-img' src={findImage(testimonial.imgTitle)} alt={testimonial.body} />
+// 					</Carousel.Item>
+// 				))}
+// 			</Carousel>
+// 		</div>
+// 	);
+// };
 
-export default Testimonials;
+// export default Testimonials;
