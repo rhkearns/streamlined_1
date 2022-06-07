34140eb214b21fe26c4ca8c0154159b334ff7c78
rhkearns <ryankearns217@gmail.com>
2022-06-07 16:38:19 -0400

    libero

--- src/common/testimonialData.js
+++ src/common/testimonialData.js
@@ -2,8 +2,14 @@ const testimonialData = [
 	{
 		name: 'Carly S.',
 		company: 'Mood Bars',
-    body: 'Samantha from Streamlined Partners is truly the best - she is proactive, organized, effective, and incredibly helpful. Working with Samantha has been seamless and incredibly helpful. She has mad my life so much easier and my business so much stronger.',
+    body: 'Samantha from Streamlined Partners is truly the best - she is proactive, organized, effective, and incredibly helpful. Working with Samantha has been seamless and incredibly helpful. She has mad my life so much easier and my business so much stronger. - Carly S., Mood Bars',
     imgTitle: "carly",
+  },
+  {
+    name: "Deborah C",
+    company: "Libero Canto",
+    body: "Hiring Samantha Yudin was the best decision I have made in years. She is organized, efficient, attentive, and extremely clear in her communications, both verbal and written. She is highly conscientious, and willing to go to admirable lengths in order to get a job done right. Samantha has been an invaluable support to me, and she is also a natural team member who seems to bring out the best in everyone. She combines genuine personal warmth with reliable professionalism. I am so grateful to have found her. - Deborah C., Libero Canto",
+    imgTitle: "liberoCanto",
   },
 ];
 
