0e10917c8a37360e6bcbece037708a295db78204
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2022-07-24 18:05:11 -0400

    packages css updates

--- src/pages/Packages/Packages.jsx
+++ src/pages/Packages/Packages.jsx
@@ -5,17 +5,20 @@ import cover_img from '../../images/cover-img.png';
 const Packages = () => {
 	return (
 		<div id="packages-page">
+		<div className="packages">
 			<img src={cover_img} alt="cover" id="cover-img" />
-			<div id="packages-section"
-			<div id="package-Header">
-			<h1>Packages</h1>
-			<div id="package-section">
-			<p>Systemization</p>
-			</div>
-			</div>
-			</div>	
-			</div>	
+					<div id="packages">
+						<h5>Packages</h5>
+						<div id="packages-section"
+						<p>test test test</p>>
+						</div>
+						</div>
+					</div>
+					
+					
+			
 		</div>	
+		
 	);
 
 };
