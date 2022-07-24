d9d4e04debac67ade3a8db2d951d5215823bef5c
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2022-07-24 18:14:09 -0400

    package 1 details

--- src/pages/Packages/Packages.jsx
+++ src/pages/Packages/Packages.jsx
@@ -8,20 +8,16 @@ const Packages = () => {
 			<img src={cover_img} alt="cover" id="cover-img" />
 			<h2 id="packages">Packages</h2>
 			<div id="packages-section">
-		<h5>Package 1</h5>
-			<p>Systemization/Base Package
-	-setting up custom domain email address
-	-Going through PM software to figure out the best fit
-	-Set up PM software, work management software, etc
-		-asana, slack, notion, trello, airtable, shared google drive or dropbox folders, milanote, figma, dubsado, 
-	-Set up password saving software
-		-recommend 1password $36/yr
-	-Set up and connect calendars
-		-calendly, doodle accounts
-	-Connect and sync socials (all handles are the same, logos, etc)
-		-Linkedin, Twitter, Facebook, Instagram TikTok
+		<h5>Systemization</h5>
+			<p>
+	Setting up custom domain email address
+	<br />Going through PM software to figure out the best fit
+	<br />Set up PM software, work management software, etc
+	<br />Set up password saving software
+	<br />Set up and connect calendars
+	<br />Connect and sync socials (all handles are the same, logos, etc)
 
-$495 for a team of up to 5 people,</p>
+$495 for a team of up to 5 people</p>
 			</div>
 			</div>	
 	);
