37fa68ed3b653f2d5bf527f6c75a2088f3d5c8c2
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2022-07-24 17:58:36 -0400

    package 1 box

--- src/pages/Packages/Packages.jsx
+++ src/pages/Packages/Packages.jsx
@@ -6,14 +6,16 @@ const Packages = () => {
 	return (
 		<div id="packages-page">
 			<img src={cover_img} alt="cover" id="cover-img" />
-
+			<div id="packages-section"
 			<div id="package-Header">
 			<h1>Packages</h1>
 			<div id="package-section">
 			<p>Systemization</p>
 			</div>
-			</div>			
-		</div>
+			</div>
+			</div>	
+			</div>	
+		</div>	
 	);
 
 };
