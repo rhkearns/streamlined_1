22acfdd7abea6c6fecad71848ec5950dc5a95a7b
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2022-07-24 21:25:02 -0400

    simplify divs

--- src/pages/Packages/Packages.jsx
+++ src/pages/Packages/Packages.jsx
@@ -7,41 +7,36 @@ const Packages = () => {
 		<div id="packages-page">
 			<img src={cover_img} alt="cover" id="cover-img" />
 			<h2 id="packages">Packages</h2>
-		<div id="packages-section">
-		<div id="systemization">
-		<h5>Systemization</h5>
-			<p>
-	Setting up custom domain email address
-	<br />Going through PM software to figure out the best fit
-	<br />Set up PM software, work management software, etc
-	<br />Set up password saving software
-	<br />Set up and connect calendars
-	<br />Connect and sync socials (all handles are the same, logos, etc)
-	<br />$495 for a team of up to 5 people</p>
-			</div>
-			</div>
-			<div id="packages-section">
+			<div className="packages-section">
+				<div id="systemization">
+					<h5>Systemization</h5>
+					<p>Setting up custom domain email address
+					<br />Going through PM software to figure out the best fit
+					<br />Set up PM software, work management software, etc
+					<br />Set up password saving software
+					<br />Set up and connect calendars
+					<br />Connect and sync socials (all handles are the same, logos, etc)
+					<br />$495 for a team of up to 5 people</p>
+				</div>
 				<div id="integration">
-		<h5>Integration and Strategy</h5>
-			<p>
-			<br />Brainstorm/touch base meetings (2 a week @ 1 hour each)
-			<br />include accountability and tracking goals
-		<br />pre-scheduled meetings
-		<br />1 hour for unscheduled meetings
-		<br />Templates and Organization Methods
-	<br />created templates, mono-working/pomodoro technique, to do lists with 
-                 deadlines at each step
-				 <br />these cover all areas of admin, accounting, PM, etc
-		<br />Can include marketing (social automation)
-		<br />Provide network of Freelancers for specialized projects
-	<br />-creative, marketing, copywriting, legal, bookkeeping, etc
-		<br />Assess needs after x amount of time for a full-time hire
-	<br />Assist in creating job description based on what is experienced
-	<br />Assist in hiring and scheduling interviews
-	<br />Work with new hire to onboard</p>
+					<h5>Integration and Strategy</h5>
+					<p>Brainstorm/touch base meetings (2 a week @ 1 hour each)
+					<br />include accountability and tracking goals
+					<br />pre-scheduled meetings
+					<br />1 hour for unscheduled meetings
+					<br />Templates and Organization Methods
+					<br />created templates, mono-working/pomodoro technique, to do lists wit deadlines at each step
+					<br />these cover all areas of admin, accounting, PM, etc
+					<br />Can include marketing (social automation)
+					<br />Provide network of Freelancers for specialized projects
+					<br />creative, marketing, copywriting, legal, bookkeeping, etc
+					<br />Assess needs after x amount of time for a full-time hire
+					<br />Assist in creating job description based on what is experienced
+					<br />Assist in hiring and scheduling interviews
+					<br />Work with new hire to onboard</p>
+				</div>
 			</div>
-			</div>
-			</div>	
+		</div>	
 	);
 
 };
