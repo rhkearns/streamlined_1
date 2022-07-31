a8ba05fe79d8e7377e65a188521dc82b5cb1615c
rhkearns <ryankearns217@gmail.com>
2022-07-31 16:26:56 -0400

    hide packages route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header/Header';
 import Home from './pages/Home/Home';
-import Packages from './pages/Packages/Packages';
+// import Packages from './pages/Packages/Packages';
 
 function App() {
 	return (
@@ -11,7 +11,7 @@ function App() {
 			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/packages" element={<Packages />} />
+				{/* <Route path="/packages" element={<Packages />} /> */}
 			</Routes>
 		</div>
 	);
