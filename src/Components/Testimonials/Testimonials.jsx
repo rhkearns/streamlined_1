bfba2284e17a7415b46822ee5aee0b476f63754a
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2022-08-08 10:25:13 -0400

    carousel interval

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -25,7 +25,7 @@ const Testimonials = () => {
 			<h1>Testimonials</h1>
 			<Carousel variant="dark">
 				{testimonialData.map(testimonial => (
-					<Carousel.Item>
+					<Carousel.Item interval={500}>
 						<img className='testimonial-img' src={findImage(testimonial.imgTitle)} alt={testimonial.body} />
 					</Carousel.Item>
 				))}
