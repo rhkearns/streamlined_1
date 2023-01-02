695fca05553f2b48a613f9dd8f440594cdc5638f
rhkearns <ryankearns217@gmail.com>
2023-01-02 14:01:16 -0500

    finalize spacing trial 1

--- src/pages/HomeRedo/HomeRedo.jsx
+++ src/pages/HomeRedo/HomeRedo.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import Testimonials from '../../Components/Testimonials/Testimonials';
+// import Testimonials from '../../Components/Testimonials/Testimonials';
 import Brief from '../../Components/Brief/Brief';
 import Process from '../../Components/Process/Process';
 import Issues from '../../Components/Issues/Issues';
 
 const HomeRedo = () => {
-  const isMobile = window.screen.width < 600;
+  // const isMobile = window.screen.width < 600;
 
   return (
     <div>
       <Brief />
       <Process />
       <Issues />
-      {!isMobile && <Testimonials id="testimonials-to-hide" />}
+      {/* {!isMobile && <Testimonials id="testimonials-to-hide" />} */}
     </div>
   )
 }
