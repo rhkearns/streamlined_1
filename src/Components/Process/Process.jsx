d9b63cc6b646232b4984dd37058fc82ad510894f
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2023-01-04 16:03:43 -0500

    process copy updates

--- src/Components/Process/Process.jsx
+++ src/Components/Process/Process.jsx
@@ -6,11 +6,12 @@ const Process = () => {
 		<div id="process-section">
 			<h1 id="process-heading">Our Process</h1>
 			<p id="process-paragraph">
-				We offer a free consultation, normally about 10 minutes, to make sure we
+				We offer a free consultation, typically about 10 minutes, to ensure we
 				are a good fit to work together. <br></br>
 				We will then provide you with a proposal, which includes the project rate
 				and timeline.
 			</p>
+			<h1 id="process-heading">Administrative Roadmap:</h1>
 			<div id="process-steps">
 				<div className="process-step-box" id="process-step-1">
 					<h2 className="process-step-number">1</h2>
@@ -28,7 +29,7 @@ const Process = () => {
 				<div className="process-step-box" id="process-step-3">
 					<h2 className="process-step-number">3</h2>
 					<p className="process-step-description">
-						Implement items outlined in the roadmap, either on your own or with our assistance!
+					Implement items outlined in the roadmap on your own or with our assistance!
 					</p>
 				</div>
 			</div>
