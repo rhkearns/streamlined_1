23fe4efacd9334627e18b4ea7ab17b60ff607dca
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2023-01-04 15:59:42 -0500

    brief copy update

--- src/Components/Brief/Brief.jsx
+++ src/Components/Brief/Brief.jsx
@@ -5,18 +5,23 @@ const Brief = () => {
 	return (
 		<div id="brief-section">
 			<p>
-				A boutique operations consulting firm helping small business owners and
+				A boutique operations consulting firm dedicated to helping small business owners and
 				entrepreneurs achieve their business goals by guiding them
 				through all aspects of the administrative and operational processes.
       </p>
       <p>
-        The amount of technology, programs, and tools on the market today can be overwhelming and make your day-to-day unnecessarily complicated. That's where we come in.
+        The amount of technology, programs, and tools on the market today can be 
+        overwhelming and make your day-to-day unnecessarily complicated. That's where we come in.
       </p>
       <p>
-        We will meet with you, take an in-depth look at your business and practices, and return a detailed, custom-tailored roadmap for your business.
+        To begin, we will meet with you, take an in-depth look at your business and practices, and return 
+        a detailed, custom-tailored roadmap for your business.
       </p>
       <p>
-        This will allow you to build a strong foundation and prepare you for success. We want you to be able to focus on the core ideas of your business, rather than spending your time thinking about how to best communicate with clients and vendors, organize files, manage projects, etc.
+        This will allow you to build a strong foundation and increase productivity and efficiency.
+        We want you to be able to focus on the core ideas of your business rather than 
+        spending your time thinking about how to best communicate with clients and 
+        vendors, organize files, manage projects, etc.
       </p>
 		</div>
 	);
