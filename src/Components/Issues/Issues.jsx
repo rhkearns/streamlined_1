c666a175d9e13fb51fed70e1b8dd4d3354f383e1
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2024-06-21 10:31:30 -0400

    Hide issues

--- src/Components/Issues/Issues.jsx
+++ src/Components/Issues/Issues.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import './Issues.scss';
 
 const Issues = () => {
-  return (
-    <div id="issues-section">
-      <h1 id="issues-header">We work through an array of administrative issues including:</h1>
-      <div id="issues-blocks-section">
-        <h1 className='issue-block'>Custom Email Domains</h1>
-        <h1 className='issue-block'>File Organization</h1>
-        <h1 className='issue-block'>Communication Tools</h1>
-        <h1 className='issue-block'>Project Management Software</h1>
-        <h1 className='issue-block'>Customer Relationship Management Tools</h1>
-        <h1 className='issue-block'>Information Sharing</h1>
-        <h1 className='issue-block'>Cross-Channel Organization</h1>
-        <h1 className='issue-block'>New Hire Prep and Onboarding</h1>
-        <h1 className='issue-block'>Goal Tracking</h1>
-        <h1 className='issue-block'>And so much more...</h1>
-      </div>
-    </div>
+  // return (
+  //   <div id="issues-section">
+  //     <h1 id="issues-header">We work through an array of administrative issues including:</h1>
+  //     <div id="issues-blocks-section">
+  //       <h1 className='issue-block'>Custom Email Domains</h1>
+  //       <h1 className='issue-block'>File Organization</h1>
+  //       <h1 className='issue-block'>Communication Tools</h1>
+  //       <h1 className='issue-block'>Project Management Software</h1>
+  //       <h1 className='issue-block'>Customer Relationship Management Tools</h1>
+  //       <h1 className='issue-block'>Information Sharing</h1>
+  //       <h1 className='issue-block'>Cross-Channel Organization</h1>
+  //       <h1 className='issue-block'>New Hire Prep and Onboarding</h1>
+  //       <h1 className='issue-block'>Goal Tracking</h1>
+  //       <h1 className='issue-block'>And so much more...</h1>
+  //     </div>
+  //   </div>
     
   )
 }
