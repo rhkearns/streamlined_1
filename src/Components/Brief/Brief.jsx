588109e8dc56647944168bf5ab2852d05f21f089
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2024-06-21 09:40:43 -0400

    brief update

--- src/Components/Brief/Brief.jsx
+++ src/Components/Brief/Brief.jsx
@@ -5,7 +5,7 @@ const Brief = () => {
 	return (
 		<div id="brief-section">
 			<p>
-				A boutique operations consulting firm dedicated to helping small business owners and
+				We are a boutique operations consulting firm dedicated to helping small business owners and
 				entrepreneurs achieve their business goals by guiding them
 				through all aspects of the administrative and operational processes.
       </p>
@@ -13,11 +13,18 @@ const Brief = () => {
         The amount of technology, programs, and tools on the market today can be 
         overwhelming and make your day-to-day unnecessarily complicated. That's where we come in.
       </p>
-      <p>
+      {/* <p>
         To begin, we will meet with you, take an in-depth look at your business and practices, and return 
         a detailed, custom-tailored roadmap for your business.
-      </p>
+      </p> */}
       <p>
+      Applying our team's project management and administrative knowledge and 
+       expertise, we will take an in-depth look at your business and practices 
+       and provide you with recommended process improvements. Our team will work 
+       with you to implement new workflows with step-by-step guides, workflow 
+       documentation, and custom templates.
+        </p>
+        <p>
         This will allow you to build a strong foundation and increase productivity and efficiency.
         We want you to be able to focus on the core ideas of your business rather than 
         spending your time thinking about how to best communicate with clients and 
