2237c5fc24522ecc723f8371bad8f980d6f45bf7
Syudin24 <105463906+Syudin24@users.noreply.github.com>
2025-05-23 12:23:18 -0400

    coming soon update 2

--- src/pages/HomeRedo/HomeRedo.jsx
+++ src/pages/HomeRedo/HomeRedo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 // import Testimonials from '../../Components/Testimonials/Testimonials';
-import Brief from '../../Components/Brief/Brief';
-import Process from '../../Components/Process/Process';
+// import Brief from '../../Components/Brief/Brief';
+// import Process from '../../Components/Process/Process';
 // import Issues from '../../Components/Issues/Issues';
-import NextSteps from '../../Components/NextSteps/NextSteps';
+// import NextSteps from '../../Components/NextSteps/NextSteps';
 
 const HomeRedo = () => {
   // const isMobile = window.screen.width < 600;
